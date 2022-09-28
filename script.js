1eb57084f40f8942a3ab73f316698e0079daa1b7
Rodrigo Denner <rodrigodenner.art@gmail.com>
2022-09-28 03:13:47 -0300

    projeto flexbox

---
+++ script.js
@@ -0,0 +1,15 @@
+function clickMenu() {
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
+    }
+    
+}
+function mudouTamanho(){
+    if(window.innerWidth >= 768){
+    itens.style.display = 'block'}
+    else{
+        itens.style.display = 'none'
+    }
+}
