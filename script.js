edb0fa9f91bb99aa34b8fa30c482391c4821d309
Rodrigo Denner <rodrigodenner.art@gmail.com>
2022-09-28 19:43:57 -0300

    manutenção no projeto

--- script.js
+++ script.js
@@ -6,10 +6,3 @@ function clickMenu() {
     }
     
 }
-function mudouTamanho(){
-    if(window.innerWidth >= 768){
-    itens.style.display = 'block'}
-    else{
-        itens.style.display = 'none'
-    }
-}
